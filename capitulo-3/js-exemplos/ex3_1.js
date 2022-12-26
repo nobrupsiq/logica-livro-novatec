93825a78b9dc5b0dc3a7c60445ec2b6aca943de4
bruno <bpires.siqueira@gmail.com>
2022-12-26 11:15:26 -0300

    capitulo 3 exemplo 3.2 peso ideal

--- capitulo-3/js-exemplos/ex3_1.js
+++ capitulo-3/js-exemplos/ex3_1.js
@@ -1,4 +1,5 @@
 function calcularMedia() {
+    // leitura dos dados
   // cria referência aos elementos da página
   var inAluno = document.getElementById("inAluno");
   var inNota1 = document.getElementById("inNota1");
@@ -6,6 +7,7 @@ function calcularMedia() {
   var outSituacao = document.getElementById("outSituacao");
   var outMedia = document.getElementById("outMedia");
 
+  // processamento
   // obtém os conteúdos dos campos de edição da página
   var nome = inAluno.value;
   var nota1 = Number(inNota1.value);
@@ -14,6 +16,7 @@ function calcularMedia() {
   // calcula a média das notas
   var media = (nota1 + nota2) / 2;
 
+  // saida dos dados
   // apresenta a média (altera o conteúdo do elemento outMedia)
   outMedia.textContent = "Média das Notas: " + media.toFixed(1);
 
@@ -21,7 +24,10 @@ function calcularMedia() {
   if(media >= 7) {
     //altera o texto e estilo da cor do elemento outSituacao
     outSituacao.textContent = 'Parabéns ' + nome + '! Você foi aprovado(a)';
-    outSituacao.style.color = 'blue';
+    outSituacao.style.color = 'green';
+  } else if (media >= 4) {
+    outSituacao.textContent = 'Atenção ' + nome + '. Você está em exame';
+    outSituacao.style.color = 'orange'
   } else {
     outSituacao.textContent = "Ops " + nome + '... Você foi reprovado(a)'
     outSituacao.style.color = 'red';
