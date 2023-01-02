e27910b5268cf8fe4548000074c51c8a636bc2c6
bruno <bpires.siqueira@gmail.com>
2023-01-02 08:50:42 -0300

    capitulo4 exercicio 4.b finalizado

--- capitulo-4/js-exercicios/criacaoDeChinchilas.js
+++ capitulo-4/js-exercicios/criacaoDeChinchilas.js
@@ -8,17 +8,20 @@ function mostrarPrevisao() {
 
   if (numero == "" || anos == "" || isNaN(numero) || isNaN(anos)) {
     alert("Preencha todos os campos corretamente...");
+    inNumero.value = '';
+    inAnos.value = '';
     inNumero.focus();
     return;
   }
 
-  var resposta = '';
-
+  var total =  numero;
+  var quantidade = '';
 
   for (var i = 1; i <= anos; i++) {
-    resposta = resposta + i + "° Ano: ";
+    quantidade += `${i}° Ano: ${total} Chinchilas. \n`
+    total = total * 3;
   }
-  outResposta.textContent = resposta;
+  outResposta.textContent = quantidade;
 }
 
 var btMostrar = document.getElementById("btMostrar");
