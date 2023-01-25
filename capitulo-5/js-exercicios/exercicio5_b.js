a695c2936ddb52e677519ddfa01095ad17d4e7cd
bruno <bpires.siqueira@gmail.com>
2023-01-25 10:16:59 -0300

    capitulo5 exercicio 5.b

--- capitulo-5/js-exercicios/exercicio5_b.js
+++ capitulo-5/js-exercicios/exercicio5_b.js
@@ -3,10 +3,11 @@ let btAdicionar = document.getElementById('btAdicionar');
 let btVerificar = document.getElementById('btVerificar');
 let outNumero = document.getElementById('outNumero');
 let outAlerta = document.getElementById('outAlerta');
+let btLimpar = document.getElementById('btLimpar');
 
 let numeroVet = []; // Vetor global
 
-function verificarNumero() {
+function adicionarNumero() {
   let numero = Number(inNumero.value);
 
   if (numero == 0 || isNaN(numero)) {
@@ -16,12 +17,39 @@ function verificarNumero() {
   }
 
   numeroVet.push(numero);
-  let lista = '';
+  let lista = 'Números: ';
 
   for (let i = 0; i < numeroVet.length; i++) {
-    lista += `Número: ${numeroVet[i]} \n`;
+    lista += `${numeroVet[i]} `;
   }
-
   outNumero.textContent = lista;
+
+  inNumero.value = '';
+  inNumero.focus();
 }
-btAdicionar.addEventListener('click', verificarNumero);
+btAdicionar.addEventListener('click', adicionarNumero);
+
+function verificarOrdem() {
+  let checarOrdem = true; // flag
+
+  for (let i = 0; i < numeroVet.length; i++) {
+    // se o numero atual do vetor for MAIOR que o PROXIMO número, a flag é alterada para false e o loop é paralizado.
+    if (numeroVet[i] > numeroVet[i + 1]) {
+      checarOrdem = false;
+      break;
+    }
+  }
+  if (checarOrdem) {
+    alert('Números em ordem crescente!');
+    inNumero.focus();
+  } else {
+    alert('Números não estão em ordem crescente!');
+    inNumero.focus();
+  }
+}
+btVerificar.addEventListener('click', verificarOrdem);
+
+function limpar() {
+  window.location.reload();
+}
+btLimpar.addEventListener('click', limpar);
